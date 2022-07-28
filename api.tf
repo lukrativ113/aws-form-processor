a29e01a69d900a5d4fe043ab318e741ed042aeb9
lukrativ113 <luc.nuytten@gmail.com>
2022-07-28 12:09:18 -0400

    adding CORS options methods

--- api.tf
+++ api.tf
@@ -20,6 +20,52 @@ resource "aws_api_gateway_method" "proxy" {
     authorization = "NONE"
 }
 
+resource "aws_api_gateway_method_response" "options" {
+    rest_api_id   = aws_api_gateway_rest_api.api_lambda.id
+    resource_id   = aws_api_gateway_method.proxy.resource_id
+    http_method   = 'OPTIONS'
+    status_code   = "200"
+
+    response_models = {
+        "application/json" = "Empty"
+    }
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true
+    }
+}
+
+resource "aws_api_gateway_integration" "options" {
+    rest_api_id   = aws_api_gateway_rest_api.api_lambda.id
+    resource_id   = aws_api_gateway_method.proxy.resource_id
+    http_method   = 'OPTIONS'
+    type             = "MOCK"
+    content_handling = "CONVERT_TO_TEXT"
+
+    request_templates = {
+        "application/json" = "{\"statusCode\": 200}"
+    }
+}
+
+resource "aws_api_gateway_integration_response" "options" {
+    rest_api_id   = aws_api_gateway_rest_api.api_lambda.id
+    resource_id   = aws_api_gateway_method.proxy.resource_id
+    http_method   = 'OPTIONS'
+    status_code   = aws_api_gateway_method_response.options.status_code
+
+    response_templates = {
+        "application/json" = ""
+    }
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
+}
+
 resource "aws_api_gateway_integration" "lambda" {
     rest_api_id      = aws_api_gateway_rest_api.api_lambda.id
     resource_id      = aws_api_gateway_method.proxy.resource_id
@@ -35,6 +81,52 @@ resource "aws_api_gateway_method" "proxy_root" {
    resource_id   = aws_api_gateway_rest_api.api_lambda.root_resource_id
    http_method   = "ANY"
    authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_root" {
+    rest_api_id   = aws_api_gateway_rest_api.api_lambda.id
+    resource_id   = aws_api_gateway_method.proxy_root.resource_id
+    http_method   = 'OPTIONS'
+    status_code   = "200"
+
+    response_models = {
+        "application/json" = "Empty"
+    }
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true
+    }
+}
+
+resource "aws_api_gateway_integration" "options_root" {
+    rest_api_id   = aws_api_gateway_rest_api.api_lambda.id
+    resource_id   = aws_api_gateway_method.proxy_root.resource_id
+    http_method   = 'OPTIONS'
+    type             = "MOCK"
+    content_handling = "CONVERT_TO_TEXT"
+
+    request_templates = {
+        "application/json" = "{\"statusCode\": 200}"
+    }
+}
+
+resource "aws_api_gateway_integration_response" "options_root" {
+    rest_api_id   = aws_api_gateway_rest_api.api_lambda.id
+    resource_id   = aws_api_gateway_method.proxy_root.resource_id
+    http_method   = 'OPTIONS'
+    status_code   = aws_api_gateway_method_response.options_root.status_code
+
+    response_templates = {
+        "application/json" = ""
+    }
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
